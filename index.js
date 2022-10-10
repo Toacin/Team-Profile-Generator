9f2d22fb2b1e84dcfb209b20ca339f3a95ab1022
Toacin <toacinp@outlook.com>
2022-10-09 21:40:23 -0700

    Unit tests now all pass

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+questions = [
+    {
+        message: "What's the team manager's name?",
+        name: "managerName"
+    },
+    {
+        message: "What's the team manager's Employee ID?",
+        name: "managerName"
+    },
+    {
+        message: "What's the team manager's email address?",
+        name: "managerName"
+    },
+    {
+        message: "What's the team manager's office ?",
+        name: "managerName"
+    },
+    {
+        message: "What's the team manager's name?",
+        name: "managerName"
+    },
+    {
+        message: "What's team manager's name?",
+        name: "managerName"
+    },
+    {
+        message: "What's your team manager's name?",
+        name: "managerName"
+    },
+    {
+        message: "What's your team manager's name?",
+        name: "managerName"
+    }
+]
